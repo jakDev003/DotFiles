d4b4c7adb72308cdebeeb7b208323fcc917daa7c
Josh Kagiwada <jkagiwada@advisor360.com>
2022-06-17 16:45:40 -0400

    updated ripgrep

--- dotfiles/nvim/lua/setup/ripgrep.lua
+++ dotfiles/nvim/lua/setup/ripgrep.lua
@@ -1,18 +1,3 @@
--- Predefined `pt` search command.
--- `pt {query} --nogroup --nocolor --smart-case`
-runner = require('nvim-ripgrep.run').pt,
-
--- Or you can define your own search command.
-runner = function(query)
-    return require('nvim-ripgrep.run').run(
-        'rg',
-        {
-            args = {
-                '--vimgrep',
-                '--smart-case',
-                '--regexp',
-                query
-            },
-        },
-    )
-end,
+require("nvim-ripgrep").setup {
+    -- your configurations here
+}
