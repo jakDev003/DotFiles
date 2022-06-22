1f542485d4967395ea1ce1e4b4154c3aeed1645b
Joshua Kagiwada <jkagiwada@advisor360.com>
2022-06-22 11:52:57 -0400

    fix autocomplete issue

--- dotfiles/nvim/lua/setup/cmp.lua
+++ dotfiles/nvim/lua/setup/cmp.lua
@@ -1,43 +1,81 @@
-local lspkind = require('lspkind')
+-- Set completeopt to have a better completion experience
+vim.opt.completeopt = 'menuone,noselect'
+
+-- nvim-cmp setup
 local cmp = require('cmp')
 cmp.setup {
-    -- As currently, i am not using any snippet manager, thus disabled it.
-       -- snippet = {
-          --   expand = function(args)
-             --     require("luasnip").lsp_expand(args.body)
-             --   end,
-          -- },
+  mapping = {
+    ['<Tab>'] = cmp.mapping.select_next_item(),
+    ['<S-Tab>'] = cmp.mapping.select_prev_item(),
+    ['<C-d>'] = cmp.mapping.scroll_docs(4),
+    ['<C-u>'] = cmp.mapping.scroll_docs(-4),
+    ['<CR>'] = cmp.mapping.confirm {
+      behavior = cmp.ConfirmBehavior.Replace,
+      select = true,
+    },
+  },
+  sources = {
+    { name = 'nvim_lsp' },
+    { name = 'buffer' },
+    { name = 'calc' },
+    { name = 'path' },
+    { name = 'spell' },
+  },
+  formatting = {
+      format = function(entry, vim_item)
+        vim_item.menu = ({
+          nvim_lsp = '[LSP]',
+          buffer = '[Buffer]',
+          calc = '[Calc]',
+          path = '[Path]',
+          spell = '[Spell]',
+        })[entry.source.name]
+        return vim_item
+      end,
+  },
+}
+
+-- ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+-- local lspkind = require('lspkind')
+-- local cmp = require('cmp')
+-- cmp.setup {
+--     -- As currently, i am not using any snippet manager, thus disabled it.
+--        -- snippet = {
+--           --   expand = function(args)
+--              --     require("luasnip").lsp_expand(args.body)
+--              --   end,
+--           -- },
  
-       mapping = {
-          ["<C-d>"] = cmp.mapping.scroll_docs(-4),
-          ["<C-f>"] = cmp.mapping.scroll_docs(4),
-          ["<C-e>"] = cmp.mapping.close(),
-          ["<c-y>"] = cmp.mapping.confirm {
-             behavior = cmp.ConfirmBehavior.Insert,
-             select = true,
-          },
-          ["<Tab>"] = cmp.mapping.confirm {
-            behavior = cmp.ConfirmBehavior.Insert,
-            select = true,
-         },
-       },
-       formatting = {
-          format = lspkind.cmp_format {
-             with_text = true,
-             menu = {
-                buffer   = "[buf]",
-                nvim_lsp = "[LSP]",
-                path     = "[path]",
-             },
-          },
-       },
+--        mapping = {
+--           ["<C-d>"] = cmp.mapping.scroll_docs(-4),
+--           ["<C-f>"] = cmp.mapping.scroll_docs(4),
+--           ["<C-e>"] = cmp.mapping.close(),
+--           ["<c-y>"] = cmp.mapping.confirm {
+--              behavior = cmp.ConfirmBehavior.Insert,
+--              select = true,
+--           },
+--           ["<Tab>"] = cmp.mapping.confirm {
+--             behavior = cmp.ConfirmBehavior.Insert,
+--             select = true,
+--          },
+--        },
+--        formatting = {
+--           format = lspkind.cmp_format {
+--              with_text = true,
+--              menu = {
+--                 buffer   = "[buf]",
+--                 nvim_lsp = "[LSP]",
+--                 path     = "[path]",
+--              },
+--           },
+--        },
  
-       sources = {
-          { name = "nvim_lsp"},
-          { name = "path" },
-          { name = "buffer" , keyword_length = 5},
-       },
-       experimental = {
-          ghost_text = true
-       }
- }
+--        sources = {
+--           { name = "nvim_lsp"},
+--           { name = "path" },
+--           { name = "buffer" , keyword_length = 5},
+--        },
+--        experimental = {
+--           ghost_text = true
+--        }
+--  }
